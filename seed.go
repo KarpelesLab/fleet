a4f367039b73af6ae7e1439abcd26a4daac91360
Mark Karpeles <magicaltux@gmail.com>
2018-08-01 10:35:08 +0900

    update seed to use struct for atomic check & update

--- seed.go
+++ seed.go
@@ -12,62 +12,72 @@ import (
 	"github.com/google/uuid"
 )
 
+type seedData struct {
+	seed []byte
+	Id   uuid.UUID
+	ts   time.Time
+}
+
 var (
-	seed   []byte
-	seedId uuid.UUID
-	ts     time.Time
+	// use a pointer for atomic seed details update
+	seed *seedData
 
 	uuidSeedidSpace = uuid.Must(uuid.Parse(UUID_SEEDID_SPACE))
 )
 
 const UUID_SEEDID_SPACE = "da736663-83ec-46ef-9c29-3f9102c5c519"
 
+func makeSeed(s []byte, t time.Time) *seedData {
+	seedId := uuid.NewHash(sha3.New256(), uuidSeedidSpace, s, 6) // uuid v6 - this is not in uuid specifications
+	return &seedData{
+		seed: s,
+		Id:   seedId,
+		ts:   t,
+	}
+}
+
 func initSeed() {
-	seed = make([]byte, 128)
-	_, err := rand.Read(seed)
+	s := make([]byte, 128)
+	_, err := rand.Read(s)
 	if err != nil {
 		panic(fmt.Sprintf("failed to initialize fleet seed: %s", err))
 	}
-	ts = time.Now()
-	updateSeedId()
+
+	seed = makeSeed(s, time.Now())
 
 	log.Printf("[fleet] Initialized with cluster seed ID = %s", SeedId())
 }
 
-func updateSeedId() {
-	seedId = uuid.NewHash(sha3.New256(), uuidSeedidSpace, seed, 6) // uuid v6 - this is not in uuid specifications
-}
-
 func SeedId() uuid.UUID {
-	return seedId
+	return seed.Id
 }
 
 func seedPacket() *Packet {
 	pkt := &Packet{Type: P_SEED}
 	pkt.SetPayload(PacketSeed{
-		Seed: seed,
-		Time: ts,
+		Seed: seed.seed,
+		Time: seed.ts,
 	})
 	return pkt
 }
 
 func handleNewSeed(s []byte, t time.Time) error {
-	if t.After(ts) {
+	cur := seed
+	if t.After(cur.ts) {
 		// time is more recent, ignore seed
 		return nil
 	}
-	if bytes.Compare(s, seed) == 0 {
+	if bytes.Compare(s, cur.seed) == 0 {
 		return nil // same seed already
 	}
-	if t == ts {
+	if t == cur.ts {
 		// not same seed, but same time, compare seeds
-		if bytes.Compare(s, seed) > 0 {
+		if bytes.Compare(s, cur.seed) > 0 {
 			// s is larger, keep seed
 			return nil
 		}
 	}
-	copy(seed, s)
-	updateSeedId()
+	seed = makeSeed(s, t)
 	log.Printf("[fleet] Updated seed from peer, new seed ID = %s", SeedId())
 	return nil
 }
