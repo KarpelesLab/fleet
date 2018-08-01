96fdc17862c32d58fbf65ab60ea3768cca27c6ba
Mark Karpeles <magicaltux@gmail.com>
2018-08-01 22:27:48 +0900

    adding seedcrypt & seeddecrypt

--- seed.go
+++ seed.go
@@ -2,9 +2,12 @@ package fleet
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/hmac"
 	"crypto/rand"
 	"fmt"
+	"io"
 	"log"
 	"time"
 
@@ -57,6 +60,45 @@ func SeedSign(in []byte) []byte {
 	hmac := hmac.New(sha3.New256, seed.seed)
 	hmac.Write(in)
 	return hmac.Sum([]byte{})
+}
+
+func SeedCrypt(in []byte) ([]byte, error) {
+	block, err := aes.NewCipher(seed.seed[:32])
+	if err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, in, nil)
+	return append(nonce, ciphertext...), nil
+}
+
+func SeedDecrypt(in []byte) ([]byte, error) {
+	block, err := aes.NewCipher(seed.seed[:32])
+	if err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext, err := aesgcm.Open(nil, in[:aesgcm.NonceSize()], in[aesgcm.NonceSize():], nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
 }
 
 func seedPacket() *Packet {
