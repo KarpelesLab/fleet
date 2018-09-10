ce99e624bc5a21cf4fac6e25a6b9f17a069e05b8
Mark Karpeles <magicaltux@gmail.com>
2018-09-10 15:50:33 +0900

    use cache dir

--- init.go
+++ init.go
@@ -2,6 +2,7 @@ package fleet
 
 // perform init of various elements in order
 func init() {
+	initPath()
 	initLog()
 	initSeed()
 	initAgent()
---
+++ path.go
@@ -0,0 +1,37 @@
+package fleet
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/TrisTech/goupd"
+)
+
+func initPath() {
+	if goupd.PROJECT_NAME != "unconfigured" {
+		// chdir to cache
+		c := GetCacheDir()
+		inf, err := os.Stat(c)
+		if err != nil && os.IsNotExist(err) {
+			err = os.MkdirAll(c, 0755)
+			if err != nil {
+				log.Printf("failed to create cache dir: %s", err)
+				return
+			}
+		} else if err != nil {
+			log.Printf("failed to access cache: %s", err)
+			return
+		}
+		if !inf.IsDir() {
+			log.Printf("error: cache is not a directory")
+			return
+		}
+		log.Printf("set cache dir: %s", c)
+		os.Chdir(c)
+	}
+}
+
+func GetCacheDir() string {
+	return filepath.Join(cacheFolder, goupd.PROJECT_NAME)
+}
